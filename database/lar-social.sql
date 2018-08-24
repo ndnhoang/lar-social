0d83e86a49b760cf98049731fcc981b5fd7e4523
Hoang <nhathoang10217@gmail.com>
2018-08-24 17:48:21 +0700

    Full code

--- database/lar-social.sql
+++ database/lar-social.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Aug 23, 2018 at 05:55 PM
+-- Generation Time: Aug 24, 2018 at 05:47 PM
 -- Server version: 5.7.22-0ubuntu0.17.10.1
 -- PHP Version: 7.1.17-0ubuntu0.17.10.1
 
@@ -23,6 +23,29 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `likes`
+--
+
+CREATE TABLE `likes` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `user_id` int(11) NOT NULL,
+  `post_id` int(11) NOT NULL,
+  `like` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `likes`
+--
+
+INSERT INTO `likes` (`id`, `created_at`, `updated_at`, `user_id`, `post_id`, `like`) VALUES
+(6, '2018-08-24 01:52:56', '2018-08-24 01:54:37', 1, 6, 0),
+(7, '2018-08-24 01:54:24', '2018-08-24 01:54:33', 1, 2, 1);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `migrations`
 --
 
@@ -38,7 +61,8 @@ CREATE TABLE `migrations` (
 
 INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (1, '2018_08_23_041017_create_users_table', 1),
-(2, '2018_08_23_101621_create_posts_table', 2);
+(2, '2018_08_23_101621_create_posts_table', 2),
+(3, '2018_08_24_075435_create_likes_table', 3);
 
 -- --------------------------------------------------------
 
@@ -60,9 +84,8 @@ CREATE TABLE `posts` (
 
 INSERT INTO `posts` (`id`, `created_at`, `updated_at`, `body`, `user_id`) VALUES
 (1, '2018-08-23 03:31:54', '2018-08-23 03:31:54', 'test', 7),
-(2, '2018-08-23 03:32:41', '2018-08-23 03:32:41', 'Test 1', 1),
-(3, '2018-08-23 03:51:13', '2018-08-23 03:51:13', 'test', 7),
-(4, '2018-08-23 03:53:14', '2018-08-23 03:53:14', 'test', 7);
+(2, '2018-08-23 03:32:41', '2018-08-23 22:27:03', 'Edited Test', 1),
+(6, '2018-08-23 20:39:34', '2018-08-23 20:39:34', 'I\'m Hoang', 5);
 
 -- --------------------------------------------------------
 
@@ -85,14 +108,20 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `created_at`, `updated_at`, `email`, `first_name`, `password`, `remember_token`) VALUES
-(1, '2018-08-22 21:40:13', '2018-08-22 21:40:13', 'nhathoang10217@gmail.com', 'Hoang', '$2y$10$xMSjyOuIhSVjBZo2Bms/re3kQCRCWZIhNCEvrgcTxmiZ1OFwrPK5.', NULL),
+(1, '2018-08-22 21:40:13', '2018-08-23 23:17:41', 'nhathoang10217@gmail.com', 'Nhat Hoang', '$2y$10$xMSjyOuIhSVjBZo2Bms/re3kQCRCWZIhNCEvrgcTxmiZ1OFwrPK5.', 'Co9oCvuBOl88B0aFVvRUJHPXvSPaGXSAsb4Wv9i0Mtdj2KMZKfMKdvh694mN'),
 (3, '2018-08-23 01:02:20', '2018-08-23 01:02:20', 'hnguyen@nlstech.net', 'Hoang', '$2y$10$sHAPs02jIfAb3EA8T0eeHuv4NtJfRVJORxW0OLJoDQsqxNxavFaHa', NULL),
-(5, '2018-08-23 01:14:44', '2018-08-23 01:14:44', 'nguyendangnhathoang003@gmail.com', 'Nhat', '$2y$10$xwYQ4vYCnp4bxrTgXcjhLObQmCFogF0mjjF6JRh4y38CTz6X2cLSy', NULL),
+(5, '2018-08-23 01:14:44', '2018-08-23 01:14:44', 'nguyendangnhathoang003@gmail.com', 'Nhat', '$2y$10$xwYQ4vYCnp4bxrTgXcjhLObQmCFogF0mjjF6JRh4y38CTz6X2cLSy', 'n4xsSazhpZRJwQrVhEa6B4IeFuKJaAURa7g7bzr8VN8ZOa4Edxp7fpDsk9r0'),
 (7, '2018-08-23 01:29:03', '2018-08-23 01:29:03', 'hnguyen@gmail.com', 'Hoang', '$2y$10$/pxfvcgIEso2y4Jcnw7yHec39Da7ItJJcJPzxDBLKJ8756uTpEb7e', NULL);
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `likes`
+--
+ALTER TABLE `likes`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `migrations`
@@ -117,15 +146,20 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `likes`
+--
+ALTER TABLE `likes`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+--
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT for table `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 --
 -- AUTO_INCREMENT for table `users`
 --
